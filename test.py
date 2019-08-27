a752b82826035590966ce8ce37da5b7e59de3c56
adi-txt <adithya.met92@gmail.com>
2019-08-27 10:42:43 -0400

    requirements, test, gitignore, first push

---
+++ test.py
@@ -0,0 +1,15 @@
+import json
+
+from arena import Arena
+
+from config import ACCESS_TOKEN
+
+client = Arena(ACCESS_TOKEN)
+
+if __name__ == '__main__':
+    chan = client.channels.channel('photo-street')
+    blocks, page = chan.contents()
+
+    print(blocks[0].source)
+    print(getattr(blocks[1], 'class'))
+
