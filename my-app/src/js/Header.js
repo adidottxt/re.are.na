9ccecc33c34faa873dab8e9ef237fbed080c4c02
adi-txt <adithya.met92@gmail.com>
2019-08-29 16:50:42 -0400

    updated header to reflect new name

--- my-app/src/js/Header.js
+++ my-app/src/js/Header.js
@@ -4,7 +4,8 @@ import '../css/Header.css'
 function Header() {
     return (
         <div id='rectangle'>
-        <p id='header-title'> three blocks</p>
+        <p id='header-title'>re.are.na</p>
+        <p id='header-blocks'>3 blocks</p>
         <p id='header-author'>
           <a href='https://are.na/adi'>by adi /
           </a>
