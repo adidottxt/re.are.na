072124ba701192555c447bba5aab87d5a01d3092
adi-txt <adithya.met92@gmail.com>
2019-08-29 12:48:02 -0400

    added images to work as props

--- front/src/App.js
+++ front/src/App.js
@@ -8,11 +8,11 @@ function App() {
     return (
         <div>
             <Header />
-            <Block />
+            <Block src='https://d2w9rnfcy7mm78.cloudfront.net/4039714/original_0e0254a0a1e901e4f67aacbebd6812e6.jpg?1554784576'/>
             <Info />
-            <Block />
+            <Block src='https://d2w9rnfcy7mm78.cloudfront.net/1580881/original_eae97b74b09c9707caf977f53fe38b57.png?1515539345'/>
             <Info />
-            <Block />
+            <Block src='https://d2w9rnfcy7mm78.cloudfront.net/2619145/original_d1009818b14274917d27b5a1a1757931.jpg?1535552897'/>
             <Info />
         </div>
     )
--- front/src/Block.js
+++ front/src/Block.js
@@ -2,12 +2,13 @@ import React from "react"
 
 import "./Block.css"
 
-function Block() {
+function Block(props) {
+    console.log(props.src)
     return (
         <div id='block'>
           <img
-            alt = 'testimg'
-            src='https://d2w9rnfcy7mm78.cloudfront.net/4039714/original_0e0254a0a1e901e4f67aacbebd6812e6.jpg?1554784576'
+            src={props.src}
+            alt='testing'
           />
         </div>
     )
