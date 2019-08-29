b2e3a3f5ee2f7933c610ad6397951bb0e60cb0d4
adi-txt <adithya.met92@gmail.com>
2019-08-29 13:16:20 -0400

    images now reduce opacity on hover

--- my-app/src/Block.js
+++ my-app/src/Block.js
@@ -3,13 +3,14 @@ import React from "react"
 import "./Block.css"
 
 function Block(props) {
-    console.log(props.src)
     return (
         <div id='block'>
-          <img
-            src={props.src}
-            alt='testing'
-          />
+          <a href={props.src}>
+            <img
+              src={props.src}
+              alt='testing'
+            />
+          </a>
         </div>
     )
 }
