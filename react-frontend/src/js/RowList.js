2b90adce6937744eb0552a6025721a85a0f9020f
adi-txt <adithya.met92@gmail.com>
2019-09-12 10:43:23 -0400

    refactored RowList to be a functional component vs class component

--- react-frontend/src/js/RowList.js
+++ react-frontend/src/js/RowList.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react"
+import React from "react"
 import { gql } from 'apollo-boost'
-import { graphql } from 'react-apollo'
+// import { graphql } from 'react-apollo'
+import{ useQuery } from '@apollo/react-hooks'
 
 import MediaRow from "./MediaRow"
 import EmptyRow from "./EmptyRow"
@@ -26,60 +27,52 @@ const getBlocksDataQuery = gql`
 }
 `
 
-class RowList extends Component {
+function RowList() {
+    const { loading, error, data } = useQuery(getBlocksDataQuery);
+    if (loading) {
+        return (
+            <>
+                <EmptyRow />
+                <EmptyRow />
+                <EmptyRow />
+            </>
+        )
+    }
 
-  displayBlocks() {
-      var data = this.props.data;
-      if (data.loading) {
-          return (
-              <>
-                  <EmptyRow />
-                  <EmptyRow />
-                  <EmptyRow />
-              </>
-          )
-      } else {
-          var i;
-          var highestRequest = 0;
-          var requestId = 0;
-          for (i = 0; i < data.allBlocks.edges.length; i++) {
-              requestId = Number(data.allBlocks.edges[i].node.requestId)
-              if (requestId > highestRequest) {
-                  highestRequest = requestId;
-              }
-          }
-          return data.allBlocks.edges.map(block => {
-              requestId = block.node.requestId
-              if (requestId !== 1 && requestId > highestRequest-3) {
-                  if (block.node.blockType === 'Text') {
-                      return <TextRow
-                          linksrc={block.node.blockUrl}
-                          text={block.node.blockContent}
-                          title={block.node.blockTitle}
-                          channel={block.node.channelTitle}
-                          date={block.node.blockCreateDate}
-                      />
-                  } else if (block.node.blockUrl !== 'test') {
-                      return <MediaRow
-                          imgsrc={block.node.blockContent}
-                          linksrc={block.node.blockUrl}
-                          title={block.node.blockTitle}
-                          channel={block.node.channelTitle}
-                          date={block.node.blockCreateDate}
-                      />
-                  }
-              } return '';
-          })
-      }
-  }
+    if (error) return "Error! $(error.message)";
 
-  render() {
-    return (
-        <>
-            {this.displayBlocks()}
-        </>
-    )
-  }
+    var i;
+    var highestRequest = 0;
+    var requestId = 0;
+    for (i = 0; i < data.allBlocks.edges.length; i++) {
+        requestId = Number(data.allBlocks.edges[i].node.requestId)
+        if (requestId > highestRequest) {
+            highestRequest = requestId;
+        }
+    }
+
+    return data.allBlocks.edges.map(block => {
+        requestId = block.node.requestId
+        if (requestId !== 1 && requestId > highestRequest-3) {
+            if (block.node.blockType === 'Text') {
+                return <TextRow
+                    linksrc={block.node.blockUrl}
+                    text={block.node.blockContent}
+                    title={block.node.blockTitle}
+                    channel={block.node.channelTitle}
+                    date={block.node.blockCreateDate}
+                />
+            } else if (block.node.blockUrl !== 'test') {
+                return <MediaRow
+                    imgsrc={block.node.blockContent}
+                    linksrc={block.node.blockUrl}
+                    title={block.node.blockTitle}
+                    channel={block.node.channelTitle}
+                    date={block.node.blockCreateDate}
+                />
+            }
+        } return '';
+    })
 }
 
-export default graphql(getBlocksDataQuery)(RowList)
+export default RowList
