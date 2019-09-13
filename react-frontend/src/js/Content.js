26a15f60001d2d4bbeabeab64ef6305857080133
adi-txt <adithya.met92@gmail.com>
2019-09-13 17:29:45 -0400

    formatting on Content.js

--- react-frontend/src/js/Content.js
+++ react-frontend/src/js/Content.js
@@ -31,63 +31,63 @@ const getBlocksDataQuery = gql`
 
 function Content() {
 
-    const { loading, data, refetch, networkStatus } = useQuery(getBlocksDataQuery, {
-      notifyOnNetworkStatusChange: true,
-    });
-    const { rows, addRows, addEmptyRows } = useContext(ContentContext);
+  const { loading, data, refetch, networkStatus } = useQuery(getBlocksDataQuery, {
+    notifyOnNetworkStatusChange: true,
+  });
+  const { rows, addRows, addEmptyRows } = useContext(ContentContext);
 
-    function refetchAndReload() {
-      setLoading = true;
+  function refetchAndReload() {
+    setLoading = true;
+    requestSent = true;
+    refetch();
+  }
+
+  useEffect(() => {
+    if (loading) {
       requestSent = true;
+    }
+
+    if ((networkStatus === 4) && setLoading) {
+      var new_data = [
+        {type: 'Empty', link: '', content: '', title: '', channel: '', date: ''},
+        {type: 'Empty', link: '', content: '', title: '', channel: '', date: ''},
+        {type: 'Empty', link: '', content: '', title: '', channel: '', date: ''},
+      ];
+      addEmptyRows(new_data);
+      setLoading = false;
       refetch();
     }
 
-    useEffect(() => {
-      if (loading) {
-        requestSent = true;
-      }
+    if (!loading && requestSent) {
+      new_data = data.allBlocks.edges.slice(data.allBlocks.edges.length - 3);
+      addRows(new_data);
+      requestSent = false;
+    }
+  });
 
-      if ((networkStatus === 4) && setLoading) {
-        var new_data = [
-          {type: 'Empty', link: '', content: '', title: '', channel: '', date: ''},
-          {type: 'Empty', link: '', content: '', title: '', channel: '', date: ''},
-          {type: 'Empty', link: '', content: '', title: '', channel: '', date: ''},
-        ];
-        addEmptyRows(new_data);
-        setLoading = false;
-        refetch();
-      }
-
-      if (!loading && requestSent) {
-        new_data = data.allBlocks.edges.slice(data.allBlocks.edges.length - 3);
-        addRows(new_data);
-        requestSent = false;
-      }
-    });
-
-    return (
-      <>
-        {rows.map((row, index) => {
-          return (
-            <Row
-              key={index}
-              type={row.type}
-              link={row.link}
-              content={row.content}
-              title={row.title}
-              channel={row.channel}
-              date={row.date}
-            />
-          )}
+  return (
+    <>
+      {rows.map((row, index) => {
+        return (
+          <Row
+            key={index}
+            type={row.type}
+            link={row.link}
+            content={row.content}
+            title={row.title}
+            channel={row.channel}
+            date={row.date}
+          />
         )}
+      )}
 
-        <div id='button-div'>
-          <button id='button' onClick={() => {
-            refetchAndReload();
-          }}>Refresh</button>
-        </div>
-      </>
-    )
+      <div id='button-div'>
+        <button id='button' onClick={() => {
+          refetchAndReload();
+        }}>Refresh</button>
+      </div>
+    </>
+  )
 }
 
 export default Content
