f30948a64449a49cc83914dde934d418445e4800
adi-txt <adithya.met92@gmail.com>
2019-09-16 13:57:08 -0400

    front end refactoring

--- react-frontend/src/js/blocks/TextBlock.js
+++ react-frontend/src/js/blocks/TextBlock.js
@@ -5,9 +5,9 @@ import "../../css/TextBlock.css"
 function TextBlock(props) {
   return (
     <div id='block'>
-    <a href={props.linksrc} target="_blank" rel="noopener noreferrer">
-      <div id='text-data'>{props.text}</div>
-    </a>
+      <a href={props.linksrc} target="_blank" rel="noopener noreferrer">
+        <div id='text-data'>{props.text}</div>
+      </a>
     </div>
   )
 }
--- react-frontend/src/js/rows/Row.js
+++ react-frontend/src/js/rows/Row.js
@@ -5,19 +5,10 @@ import EmptyRow from "./EmptyRow"
 import TextRow from "./TextRow"
 
 function Row(props) {
+
   if (props.type === 'Empty') {
     return (
       <EmptyRow />
-    )
-  } else if (props.type === 'Media' || props.type === 'Link' || props.type === 'Image' || props.type === 'Attachment') {
-    return (
-      <MediaRow
-        imgsrc={props.content}
-        linksrc={props.link}
-        title={props.title}
-        channel={props.channel}
-        date={props.date}
-      />
     )
   } else if (props.type === 'Text') {
     return (
@@ -30,6 +21,16 @@ function Row(props) {
       />
     )
   }
+
+  return (
+    <MediaRow
+      imgsrc={props.content}
+      linksrc={props.link}
+      title={props.title}
+      channel={props.channel}
+      date={props.date}
+    />
+  )
 }
 
 export default Row
