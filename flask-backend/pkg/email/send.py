20481f75f68899295edc8856a38387e374bb1022
adi-txt <adithya.met92@gmail.com>
2019-09-17 13:08:39 -0400

    email branch started (for now)

---
+++ flask-backend/pkg/email/send.py
@@ -0,0 +1,34 @@
+'''
+trying another way
+'''
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from email_content import HTML, FINAL_HTML
+from config import PW, EMAIL
+
+# Create message container - the correct MIME type is multipart/alternative.
+MSG = MIMEMultipart('alternative')
+MSG['Subject'] = "Link"
+MSG['From'] = EMAIL
+MSG['To'] = EMAIL
+
+# Record the MIME types of both parts - text/plain and text/html.
+PART1 = MIMEText(FINAL_HTML, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+MSG.attach(PART1)
+
+# Send the message via local SMTP server.
+MAIL = smtplib.SMTP('smtp.gmail.com', 587)
+
+MAIL.ehlo()
+MAIL.starttls()
+
+MAIL.login(EMAIL, PW)
+MAIL.sendmail(EMAIL, EMAIL, MSG.as_string())
+MAIL.quit()
