50633aeff6a8e4904d40afedb60f32ec9b594d0f
adi-txt <adithya.met92@gmail.com>
2019-09-19 11:46:14 -0400

    restructuring

--- server/pkg/read_html.py
+++ server/pkg/read_html.py
@@ -3,5 +3,5 @@ reading in email content from .html file
 '''
 import codecs
 
-READ_HTML = codecs.open('table-final.html', 'r')
+READ_HTML = codecs.open('html/final.html', 'r')
 FINAL_HTML = READ_HTML.read()
